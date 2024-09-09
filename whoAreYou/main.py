bbff8653e1b4bd80ab3df9f650b02b6b4ab44971
pedroesouza <pedro.souza@ucas-edu.net>
2024-09-09 09:53:15 -0600

    who are you

---
+++ whoAreYou/main.py
@@ -0,0 +1,6 @@
+#Pedro Souza, Who are you?
+name = input("Hello! What is your name?\n")
+print(name, ", nice to meet you, how wold are you?")
+age = input()
+favColor = input("Cool! What's your favorite color?\n")
+print(name, "is", age, "years old and his(or her) favorite color is", favColor)
