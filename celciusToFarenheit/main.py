984f1e5a174fd80246491aeda45c4659b45b95fc
pedroesouza <pedro.souza@ucas-edu.net>
2024-09-10 09:46:42 -0600

    Celcius to Farenheit

---
+++ celciusToFarenheit/main.py
@@ -0,0 +1,4 @@
+#Pedro Souza, Celcius to Farenheit
+celcius = float(input("Enter temperature in Celcius: "))
+faranheit = (celcius * (9/5)) + 32
+print(str(celcius), "degrees celcius in farenheit is", str(faranheit))
