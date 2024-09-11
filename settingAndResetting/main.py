444cc0d8adabcb9bb7b7566d5fc7c30f41ec82db
pedroesouza <pedro.souza@ucas-edu.net>
2024-09-11 09:14:26 -0600

    setting % resetting done

--- settingAndResetting/main.py
+++ settingAndResetting/main.py
@@ -1,5 +1,13 @@
+import math
 facultyAndStaff = 32
 studentsAwarded = 100
 guests = studentsAwarded * 2
 tableSeats = 12
+board = 0
 
+studentsAwarded -= 1
+facultyAndStaff -= 3
+guests -= 15
+board += 1
+
+print("You will need", math.ceil((studentsAwarded + facultyAndStaff + guests + board) / tableSeats), "tables.")
