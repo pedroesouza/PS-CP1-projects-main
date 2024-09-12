30d2320442071054ca3599ff446e9d22283b43e2
pedroesouza <pedro.souza@ucas-edu.net>
2024-09-12 09:28:45 -0600

    finished simple calculator

--- simpleCalculator/main.py
+++ simpleCalculator/main.py
@@ -2,4 +2,11 @@
 num1 = int(input("Number 1: "))
 num2 = int(input("Number 2: "))
 
-print()
+print("")
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+print("Floor division:", num1 // num2)
