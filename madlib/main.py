a8b3c3484a1dfbc621000b641bbf02ae6a86c215
pedroesouza <pedro.souza@ucas-edu.net>
2024-09-12 09:51:53 -0600

    Madlib

---
+++ madlib/main.py
@@ -0,0 +1,10 @@
+sentient1 = input("Give me a random creature or sentient thing: ")
+sentient2 = input("Give me another random creature or sentient thing: ")
+sentient3 = input("Give me another random creature or sentient thing: ")
+adjective1 = input("Give me any adjective: ")
+adjective2 = input("Give me another adjective: ")
+name = input("Give me a name: ")
+place1 = input("Now give me a place: ")
+place2 = input("Now give me another place: ")
+
+print("Once upon a time there was a young", sentient1, "named", name, "and", name,"really enjoyed going on walks. One time while he(or she) was going on a walk he(or she) saw a very", adjective1, sentient2, "and decided to run. As he(or she) ran he(or she) saw a", adjective2, sentient3, "and he(or she) introduced herself.\nThey decided to team up and brutally murder the", sentient2, "when they found him(or her) in", place1, "they lived hapily ever after in", place2)
