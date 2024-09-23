bea2f09406bce8b9faae8827799fba2abdb9caec
pedroesouza <pedro.souza@ucas-edu.net>
2024-09-23 09:48:12 -0600

    change to 5

--- anagram/main.py
+++ anagram/main.py
@@ -3,7 +3,7 @@ import random
 name = input("Type your name: ")
 letters = list(name)
 
-for x in range(0, 6):
+for x in range(0, 5):
     random.shuffle(letters)
     newLetters = "".join(letters)
     print(newLetters)
