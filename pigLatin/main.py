884ad713907924d39bb3a75a6a2fc2c2277c469a
pedroesouza <pedro.souza@ucas-edu.net>
2024-09-25 09:00:23 -0600

    update, og letters

--- pigLatin/main.py
+++ pigLatin/main.py
@@ -4,14 +4,15 @@ vowels = ["a", "e", "i", "o", "u"]
 def translate(word):
     firstLetter = word[0]
     if firstLetter in vowels:
+        print("Original word:", word)
         word += "yay"
         print(word)
     else:
         wordList = list(word)
         wordList.append(firstLetter)
         wordList.pop(0)
+        print("Original word:", word)
         word = "".join(wordList)
-        word += "yay"
-        print(word)
-
+        word += "ay"
+        print("Pig latin translation:", word)
 translate(input("Your word: "))
