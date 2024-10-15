a8d4f55a2831ce0750ed0ce0c8593b8172df6906
pedroesouza <pedro.souza@ucas-edu.net>
2024-10-15 09:39:25 -0600

    ahfsoiadihbsrviusayfhiuzdxfupoa (authorized)

---
+++ authorized/main.py
@@ -0,0 +1,13 @@
+#Pedro Souza, Authorized
+authorized = ["Pedro", "Not Jackson", "Boe Jiden", "Kendrick Lamar", "Potato Enjoyer 3", "The Guy From Amongus", "IDK", "Olaf From Frozen", "Justin Beiber"]
+admins = ["Pedro", "Justin Bieber", "Boe Jiden"]
+user = input("Who are you?: ")
+
+if user in authorized:
+    if user in admins:
+        print("Hello admin, welcome to the crappy python project that you for some reason are an admin for.")
+    else:
+        print("You are authorized in... well... the... whatever this is.")
+else:
+    print("You're not allowed, GET OUT!?!?!?!?!?! 😠🤬😠😡😠👹😠👺😠💢💢💢")
+
