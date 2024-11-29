8aaf2bd35d3e2536d250d320d4d6de46e4471b97
pedroesouza <pedro.souza@ucas-edu.net>
2024-11-29 02:41:04 -0700

    Error handling calc

--- errorHandlingCalc/main.py
+++ errorHandlingCalc/main.py
@@ -1,58 +1,50 @@
 #Pedro Souza, Error Handling Calculator
 
-num1 = 0
-num2 = 0
-
-def ask1(a):
+def ask_o(operator):
+    
     try:
-        int(a)
+        operator = int(operator)
     except:
-        a = ask1(input(input("INVALID, number 1: ")))
+        operator = ask_o(input("INVALID! (numbers only) What operation would you like to do: addition(1), subtraction(2), multiplication(3), or division(4)?"))
 
-    else:
-        int(a)
+    return operator
 
-    return a
+def ask_n_1(num1):
 
-
-def ask2(a):
     try:
-        int(a)
+        num1 = int(num1)
     except:
-        a = ask2(input(input("INVALID, number 2: ")))
+        num1 = ask_n_1(input("INVALID! (numbers only) Number 1: "))
 
-def ask3(a):
+    return num1
+
+def ask_n_2(num2):
+
+    if num2 == "0":
+        if operator == 4:
+            num2 = ask_n_2(input("INVALID! (you can't divide by 0) Number 2: "))
+
     try:
-        int(a)
-
-        if a not in [1, 2, 3, 4, 5]:
-            num1 = ask3(input(input("INVALID, *(1), /(2), +(3). -(4), **(5): ")))
-            break
-        else:
-            return a
-
-
+        num2 = int(num2)
     except:
-        num1 = ask3(input(input("INVALID, *(1), /(2), +(3). -(4), **(5): ")))
-        break
+        num2 = ask_n_2(input("INVALID! (numbers only) Number 2: "))
 
+    return num2
 
-num1 = input("number 1: ")
-num2 = ask2(input("number 2: "))
-operator = ask3(input("*(1), /(2), +(3). -(4), **(5): "))
+operator = ask_o(input("What operation would you like to do: addition(1), subtraction(2), multiplication(3), or division(4)?"))
 
+num1 = ask_n_1(input("Number 1: "))
+
+num2 = ask_n_2(input("Number 2: "))
 
 if operator == 1:
-    print(f"Your answer is: {num1 * num2}")
+    print("The answer is:", num1+num2)
 
 if operator == 2:
-    print(f"Your answer is: {num1 / num2}")
+    print("The answer is:", num1-num2)
 
 if operator == 3:
-    print(f"Your answer is: {num1 + num2}")
+    print("The answer is:", num1*num2)
 
 if operator == 4:
-    print(f"Your answer is: {num1 - num2}")
-
-if operator == 5:
-    print(f"Your answer is: {num1 ** num2}")
+    print("The answer is:", num1/num2)
